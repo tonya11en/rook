292808e3dad624e3ecbcf9fa183c985bbb690b3a
Tony Allen <tony@upbound.io>
2018-04-24 11:53:23 -0700

    Remove unused imports.
    Signed-off-by: Tony Allen <tony@upbound.io>

--- pkg/apis/minio.rook.io/v1alpha1/types.go
+++ pkg/apis/minio.rook.io/v1alpha1/types.go
@@ -16,10 +16,7 @@ limitations under the License.
 package v1alpha1
 
 import (
-	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	rook "github.com/rook/rook/pkg/apis/rook.io/v1alpha2"
 )
 
 // ***************************************************************************
